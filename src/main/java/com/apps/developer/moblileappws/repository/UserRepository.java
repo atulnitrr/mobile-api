c69ce8d79e9bf1ace2c3964d11f8420ee07fbb89
atulnitrr <atulkumar458@gmail.com>
2019-07-01 23:59:48 +0530

    Added user repo

---
+++ src/main/java/com/apps/developer/moblileappws/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.apps.developer.moblileappws.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.apps.developer.moblileappws.entity.UserEntity;
+
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+}
